ae66488cc71a809cee7c9b24ac1f5b64d97a664b
OLLAPION <ollapion@gmail.com>
2024-09-23 15:53:22 +0200

    Base du Drawer sur le CoreActivity

--- app/src/main/java/com/example/go4lunch/ui/CoreActivity.java
+++ app/src/main/java/com/example/go4lunch/ui/CoreActivity.java
@@ -1,9 +1,15 @@
 package com.example.go4lunch.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import com.example.go4lunch.R;
 import com.example.go4lunch.ui.fragment.ListRestaurantFragment;
@@ -11,9 +17,11 @@ import com.example.go4lunch.ui.fragment.ListWorkmatesLunchWithYouFragment;
 import com.example.go4lunch.ui.fragment.MapFragment;
 import com.example.go4lunch.view.LocationViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class CoreActivity extends AppCompatActivity {
+public class CoreActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
+    private DrawerLayout drawerLayout;
     private LocationViewModel locationViewModel;
 
     @Override
@@ -21,8 +29,22 @@ public class CoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_core);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        // Setup Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        // Setup DrawerLayout and NavigationView
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Toggle pour ouvrir/fermer le Drawer
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Setup BottomNavigationView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             Fragment selectedFragment = null;
             switch (item.getItemId()) {
@@ -41,6 +63,33 @@ public class CoreActivity extends AppCompatActivity {
                     .commit();
             return true;
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Gérer les éléments du drawer
+        switch (item.getItemId()) {
+            case R.id.menu_item_profile:
+                // Ouvrir le profil
+                break;
+            case R.id.menu_item_settings:
+                // Ouvrir les paramètres
+                break;
+            case R.id.menu_item_logout:
+                // Déconnexion
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     // pour la partie GPS
